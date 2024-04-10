69fbd3dc29fc4a4d5ad57a51a77c1a362aa12fc6
jjacoboflorez95 <78884528+jjacoboflorez95@users.noreply.github.com>
2024-04-10 01:15:36 -0400

    logout service

--- lib/middlewares/index.ts
+++ lib/middlewares/index.ts
@@ -2,6 +2,7 @@ import type { Request, Response, NextFunction } from 'express';
 import { AdminService } from '../services/Admin';
 import { SuperAdminService } from '../services/SuperAdmin';
 import { httpCodes } from '../constants/http-status-code';
+import { UserService } from '../services/User';
 
 async function isAdminOrSuperAdmin(req: Request, res: Response, next: NextFunction) {
   try {
@@ -50,7 +51,45 @@ async function isSuperAdmin(req: Request, res: Response, next: NextFunction) {
   }
 }
 
+async function isBlocked(req: Request, res: Response, next: NextFunction) {
+  try {
+    // @ts-ignore
+    const userId = req.user?.userId;
+
+    if (!userId) return res.sendStatus(401);
+
+    const user = await AdminService.findById(userId);
+
+    if (!user || user.isBlocked) return res.sendStatus(401);
+
+    // @ts-ignore
+    req['currentUser'] = user;
+    next();
+  } catch (error) {
+    console.error('Error in IsBlockedGuard:', error);
+    res.sendStatus(500);
+  }
+}
+
+async function tokenBlacklist(req: Request, res: Response, next: NextFunction) {
+  try {
+    // @ts-ignore
+    const accessToken = req.user?.accessToken;
+
+    const tokenInBlackList =  await AdminService.tokenInBlackList(accessToken);
+
+    if (tokenInBlackList) return res.sendStatus(401);
+
+    next();
+  } catch (error) {
+    console.error('Error in TokenBlacklistGuard:', error);
+    res.sendStatus(500);
+  }
+}
+
 export  {
   isAdminOrSuperAdmin,
-  isSuperAdmin
+  isSuperAdmin,
+  isBlocked,
+  tokenBlacklist
 };
